85eb685b61a0ac629273580d110bcbf00117b3d3
Rafid Muhammad <rafid.muhammad@gmail.com>
2022-10-03 15:26:04 +0700

    initial commit

---
+++ lib/shared/theme.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+double defaultMargin = 24.0;
+double defaultRadius = 17.0;
+
+Color primarycolor = const Color(0xFF5C40CC);
+Color textcolor = const Color(0xFF1F1449);
+Color whitecolor = const Color(0xFFFFFFFF);
+Color greycolor = const Color(0xFF9698A9);
+Color greencolor = const Color(0xFF0EC3AE);
+Color redcolor = const Color(0xFFEB70A5);
+Color backgroundcolor = const Color(0xFFFAFAFA);
+Color inactivecolor = const Color(0xFFDBD7EC);
+Color transparentColor = Colors.transparent;
+Color unavailableColor = const Color(0xFFEBECF1);
+Color availableColor = const Color(0xFFE0D9FF);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(color: textcolor);
+TextStyle whiteTextStyle = GoogleFonts.poppins(color: whitecolor);
+TextStyle greyTextStyle = GoogleFonts.poppins(color: greycolor);
+TextStyle greenTextStyle = GoogleFonts.poppins(color: greencolor);
+TextStyle redTextStyle = GoogleFonts.poppins(color: redcolor);
+TextStyle purpleTextStyle = GoogleFonts.poppins(color: primarycolor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
